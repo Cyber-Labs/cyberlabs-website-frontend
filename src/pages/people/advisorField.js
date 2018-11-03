4d3219179269c80c7b54456ac87a96f48dd5f44f
anushkamittal <anushkashikha1998@gmail.com>
2018-11-03 21:10:54 +0530

    changes made to advisorFeild.js

--- src/pages/people/advisorField.js
+++ src/pages/people/advisorField.js
@@ -1,6 +1,18 @@
 export default  [
-    {name:"Rahul Paul", opImg:"assets/img/highlyOptimised/rahulPaul.jpg", img :"https://pay.google.com/about/static/images/social/og_image.jpg", position:"", place:"University of South Florida" },
-    {name:"Mohit Punjabi", opImg:"assets/img/highlyOptimised/mohitPun.jpg", img:"https://opensource.google.com/assets/static/images/home/blog/blog_image_1.jpg", position:"Amazon", place:"India" }
+    {name:"Mohit Chawala", opImg:"https://opensource.google.com/assets/static/images/home/blog/blog_image_1.jpg", img :"assets/img/aluminiProfile/mohit.png", position:"", place:"" },
+    //{name:"Mohit Punjabi", opImg:"assets/img/highlyOptimised/mohitPun.jpg", img:"https://opensource.google.com/assets/static/images/home/blog/blog_image_1.jpg", position:"Amazon", place:"India" }
+    {name:"Prabodh Tripathi", opImg:"https://opensource.google.com/assets/static/images/home/blog/blog_image_1.jpg", img :"assets/img/aluminiProfile/prabodh.jpg", position:"", place:"" },
+    {name:"Kriti Singh", opImg:"https://opensource.google.com/assets/static/images/home/blog/blog_image_1.jpg", img :"assets/img/aluminiProfile/kriti1.jpg", position:"", place:"" },
+    {name:"Jibin Sabu Mathews", opImg:"https://opensource.google.com/assets/static/images/home/blog/blog_image_1.jpg", img :"assets/img/aluminiProfile/jibinmathews.jpg", position:"", place:"" },
+    {name:"Nikita kapoor", opImg:"https://opensource.google.com/assets/static/images/home/blog/blog_image_1.jpg", img :"assets/img/aluminiProfile/nikitakapoor.jpg", position:"", place:"" },
+    {name:"Ankit Jaiswal", opImg:"https://opensource.google.com/assets/static/images/home/blog/blog_image_1.jpg", img :"assets/img/aluminiProfile/ankit.png", position:"", place:"" },
+    {name:"Aashish Mohan Verma", opImg:"https://opensource.google.com/assets/static/images/home/blog/blog_image_1.jpg", img :"assets/img/aluminiProfile/ashishverma.jpg", position:"", place:"" },
+    {name:"Janpreet Singh", opImg:"https://opensource.google.com/assets/static/images/home/blog/blog_image_1.jpg", img :"assets/img/aluminiProfile/janpreet.png", position:"", place:"" },
+    {name:"Venkat Bulusu", opImg:"https://opensource.google.com/assets/static/images/home/blog/blog_image_1.jpg", img :"assets/img/aluminiProfile/venkat.jpg", position:"", place:"" },
+    {name:"Medha Bajpai", opImg:"https://opensource.google.com/assets/static/images/home/blog/blog_image_1.jpg", img :"assets/img/aluminiProfile/medha.jpg", position:"", place:"" },
+    {name:"Abdur Rehman Danish", opImg:"https://opensource.google.com/assets/static/images/home/blog/blog_image_1.jpg", img :"assets/img/aluminiProfile/abdur.jpg", position:"", place:"" },
+    {name:"Ishu Ratna", opImg:"https://opensource.google.com/assets/static/images/home/blog/blog_image_1.jpg", img :"assets/img/aluminiProfile/ishu.jpg", position:"", place:"" },
+    {name:"Sambuddha Nath", opImg:"https://opensource.google.com/assets/static/images/home/blog/blog_image_1.jpg", img :"assets/img/aluminiProfile/sam.jpg", position:"", place:"" },
 ]
 
 
