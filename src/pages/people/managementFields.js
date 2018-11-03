2644c2810c5310e7498391fb5c0b8919c97cb054
anushkamittal <anushkashikha1998@gmail.com>
2018-11-03 21:28:34 +0530

    sachin sir's photo added

--- src/pages/people/managementFields.js
+++ src/pages/people/managementFields.js
@@ -1,5 +1,5 @@
 let management =  [
-    {name:"Prof. Sachin Tripathi", img:"kalyan_sir.jpg", position:"President" },
+    {name:"Prof. Sachin Tripathi", img:"sachin-tripathi.png", position:"President" },
     {name:"Mohit Chawla", img:"mohit.png", position:"Founder" },
     {name:"Naveen Jain", img:"./memberProfile/naveen-jain.jpg", position:"Secretary" }
 ]
