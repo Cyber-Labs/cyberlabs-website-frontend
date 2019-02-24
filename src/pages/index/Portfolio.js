ef8f3edea4e2425734d342e20f23adc72d13342e
AnuragGupta93 <2893ag@gmail.com>
2019-02-24 10:53:55 +0530

    mearged request

--- src/pages/index/Portfolio.js
+++ src/pages/index/Portfolio.js
@@ -17,19 +17,19 @@ class Portfolio extends Component {
               />
               <ul className="details">
                 <li className="author">{name}</li>
+                <h1 className="icons-font">
+                  <a href={`${links.github}`}>
+                    <i
+                      className="fab icons-font-padding fa fa-github fa-3px"
+                      style={{ float: "right" }}
+                    />
+                  </a>
+                </h1>
               </ul>
             </div>
             <div className="description">
               <h1>{project}</h1>
               <p>{description}</p>
-              <h1 className="icons-font">
-                <a href={`${links.github}`}>
-                  <i
-                    className="fab icons-font-padding fa fa-github fa-3px"
-                    style={{ float: "right" }}
-                  />
-                </a>
-              </h1>
             </div>
           </div>
         );
@@ -38,18 +38,21 @@ class Portfolio extends Component {
   }
   render() {
     return (
-    <div>
-    <div id="Portfolio">{this.renderProjects()}
-    </div>
-    <div className="content" id="ajax-content">
-              <div className="text-intro" id="site-type">
-              <div>
-                  <a className="indexLinks" href="projects" style={{width:"40%"}}>
-                    <h3 className="card-heading">See projects ></h3>
-                  </a>
-                </div>
-              </div>
+      <div>
+        <div id="Portfolio">{this.renderProjects()}</div>
+        <div className="content" id="ajax-content">
+          <div className="text-intro" id="site-type">
+            <div>
+              <a
+                className="indexLinks"
+                href="projects"
+                style={{ width: "40%" }}
+              >
+                <h3 className="card-heading">See projects ></h3>
+              </a>
             </div>
+          </div>
+        </div>
       </div>
     );
   }
