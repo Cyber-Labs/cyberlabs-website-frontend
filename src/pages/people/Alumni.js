30649fc40e135488604506532d7a035ef2dc75c3
aayushdutt <aayushd100@gmail.com>
2019-08-31 06:32:15 +0530

    add hover on alumni cards, add the bit theories link

--- src/pages/people/Alumni.js
+++ src/pages/people/Alumni.js
@@ -1,58 +1,59 @@
 import React, { Component } from "react";
 import alumni from "./../../json-files/alumni.json";
+import "./Alumni.css";
 import ProgressiveImage from "react-progressive-image";
 class Alumni extends Component {
-  
   componentDidMount() {
     window.scrollTo(0, 0);
-    document.title="Cyber Labs|Alumni";
-    document.querySelector("meta[name='keywords']").content="Alumni, Cyber Labs, cyberlabs alumni,ism alumni,cyber labs alumni";
-    document.querySelector("meta[name='description']").content="Cyber Labs Alumni";
+    document.title = "Cyber Labs|Alumni";
+    document.querySelector("meta[name='keywords']").content =
+      "Alumni, Cyber Labs, cyberlabs alumni,ism alumni,cyber labs alumni";
+    document.querySelector("meta[name='description']").content =
+      "Cyber Labs Alumni";
   }
 
   renderFields(input) {
-    return alumni.map(({ name, img, position, place, opImg ,yearOfGrad,links}) => {
-      if(yearOfGrad===input){
-        return (
-          <div>
-          <a href={links ? links.linkedin: "#"}>
-          <div className="grid-responsive">
-            <ProgressiveImage
-              src={"/assets/img/alumni/" + img}
-              placeholder={"/assets/img/default-profile-icon.jpg"}
-            >
-              {(src, loading) => (
-                <img
-                  src={src}
-                  alt="alumni"
-                  height="230"
-                  width="250px"
-                  style={{
-                    borderRadius: "10px",
-                    objectFit: "cover",
-                    objectPosition: "50% 20%",
-                    filter: loading ? "blur(8px)" : "blur(0)"
-                  }}
-                />
-              )}
-            </ProgressiveImage>
-            <center>
-              <h4 className="person-name">{name}</h4>
-              <h6 className="person-position">
-                {position}
-                <br />
-                {place}
-              </h6>
-            </center>
-          </div>
-          </a>
-          </div>
-        );
+    return alumni.map(
+      ({ name, img, position, place, opImg, yearOfGrad, links }) => {
+        if (yearOfGrad === input) {
+          return (
+            <div>
+              <a href={links ? links.linkedin : "#"}>
+                <div className="grid-responsive">
+                  <ProgressiveImage
+                    src={"/assets/img/alumni/" + img}
+                    placeholder={"/assets/img/default-profile-icon.jpg"}
+                  >
+                    {(src, loading) => (
+                      <img
+                        src={src}
+                        alt="alumni"
+                        height="230"
+                        width="250px"
+                        style={{
+                          filter: loading ? "blur(8px)" : "blur(0)",
+                          objectFit: "cover"
+                        }}
+                      />
+                    )}
+                  </ProgressiveImage>
+                  <center>
+                    <h4 className="person-name">{name}</h4>
+                    <h6 className="person-position">
+                      {position}
+                      <br />
+                      {place}
+                    </h6>
+                  </center>
+                </div>
+              </a>
+            </div>
+          );
+        } else {
+          return null;
+        }
       }
-      else{
-        return null;
-      }
-    });
+    );
   }
 
   render() {
